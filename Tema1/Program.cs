b3c71e73d4d87c9b3ac4d7a20bc3c57d5cd2b2ff
MateiCiobanu <matei0405@gmail.com>
2025-10-10 11:12:38 +0300

    added small comments

--- Tema1/Program.cs
+++ Tema1/Program.cs
@@ -1,11 +1,14 @@
 ﻿using Tema1;
 
+// Creez niste carti 
 var book1 = new Book("1984", "George Orwell", 1949);
 var book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
 var book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
 
+// Creez un borrower
 var borrower1 = new Borrower(1, "Matei", new List<Book> { book1, book2 });
 
+// Clonare borrower cu with
 var borrower2 = borrower1 with
 {
     BorrowedBooks = new List<Book>(borrower1.BorrowedBooks)
@@ -17,10 +20,10 @@ var borrower2 = borrower1 with
 Console.WriteLine($"{borrower2.Name} a împrumutat {borrower2.BorrowedBooks.Count} cărți: {string.Join(", ", borrower2.BorrowedBooks.Select(b => b.Title))}");
 
 
-
-// Top-level statements
+// Creez o lista in care salvez listele primite ca input de la utilizator
 var userGivenBooks = new List<Book>();
 
+// Bucla while in care primesc input de la utilizator, pana cand acesta apasa doar Enter
 while (true)
 {
     Console.Write("Introdu titlul unei cărți (sau apasă Enter ca să oprești): ");
@@ -49,13 +52,12 @@ foreach (var book in userGivenBooks)
     Console.WriteLine($"- {book.Title} ({book.YearPublished}) — {book.Author}");
 }
 
-// ----------------------------------------------------------------------
-
+// Testez metoda DisplayInfo
 Console.WriteLine("\nTest DisplayInfo:");
 DisplayHelper.DisplayInfo(book1);
 DisplayHelper.DisplayInfo(borrower2);
 DisplayHelper.DisplayInfo(12345);
 
 
-//----------------------------------------------------------------------
+// Testez metoda DisplayBooksAfter2010
 DisplayHelper.DisplayBooksAfter2010(userGivenBooks);
